98749c6ec453e43c61c52652342d71b6c74205dd
yyc <1185553559@qq.com>
2023-11-04 17:06:06 +0800

    创新点

--- email_content_generator.py
+++ email_content_generator.py
@@ -1,9 +1,8 @@
 def create_grades_entry(df, student_id):
     # 根据学生ID筛选出该生的所有成绩
     student_grades = df[df['学号'] == student_id]
-
-    # 创建成绩条目的HTML列表
-    grades_entry = '<ul>'
+    # 创建成绩条目的列表
+    grades_entry = []
     for _, row in student_grades.iterrows():
         # 检查成绩是否为数值类型
         if isinstance(row['百分成绩'], (int, float)):
@@ -11,31 +10,17 @@ def create_grades_entry(df, student_id):
         else:
             # 非数值类型的成绩直接转换为字符串
             grade = str(row['百分成绩'])
-        grades_entry += f"<li>{row['课程名称']}: {grade}</li>"
-    grades_entry += '</ul>'
+        grades_entry.append(f"{row['课程名称']}: {grade}")
     return grades_entry
 
-def generate_email_content(student_id, student_name, grade_entries):
-    # 将成绩条目转换为 HTML 格式的字符串
-    grades_str = grade_entries 
-    email_content = f"""
-    <html>
-    <head>
-        <style>
-            body {{ font-family: Arial, sans-serif; }}
-            li {{ margin-bottom: 5px; }}
-        </style>
-    </head>
-    <body>
-        <p>亲爱的<strong>{student_id}{student_name}</strong>同学:</p>
-        <p>祝贺您顺利完成本学期的学习！教务处在此向您发送最新的成绩单。</p>
-        {grades_str}
-        <p>希望您能够对自己的成绩感到满意，并继续保持努力和积极的学习态度。如果您在某些科目上没有达到预期的成绩，不要灰心，这也是学习过程中的一部分。我们鼓励您与您的任课教师或辅导员进行交流，他们将很乐意为您解答任何疑问并提供帮助。请记住，学习是一个持续不断的过程，我们相信您有能力克服困难并取得更大的进步。</p>
-        <p>再次恭喜您，祝您学习进步、事业成功！</p>
-        <p>教务处</p>
-    </body>
-    </html>
-    """
+
+def generate_email_content(student_name, grade_entries):
+    # 根据学生姓名和成绩条目生成邮件内容
+    grades_str = "\n".join(grade_entries)
+    email_content = f"亲爱的{student_name}同学:\n\n" \
+                    f"祝贺您顺利完成本学期的学习！教务处在此向您发送最新的成绩单。\n\n" \
+                    f"{grades_str}\n\n" \
+                    "希望您能够对自己的成绩感到满意，并继续保持努力和积极的学习态度。如果您在某些科目上没有达到预期的成绩，不要灰心，这也是学习过程中的一部分。我们鼓励您与您的任课教师或辅导员进行交流，他们将很乐意为您解答任何疑问并提供帮助。请记住，学习是一个持续不断的过程，我们相信您有能力克服困难并取得更大的进步。\n\n" \
+                    "再次恭喜您，祝您学习进步、事业成功！\n\n" \
+                    "教务处"
     return email_content
-
-
